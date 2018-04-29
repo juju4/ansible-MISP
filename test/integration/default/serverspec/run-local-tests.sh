f4674515d627555913c552092e553f07ea3e2efa
juju4 <juju4@users.noreply.github.com>
2018-04-28 20:52:49 -0400

    refresh manual serverspec: rvmsudo

--- test/integration/default/serverspec/run-local-tests.sh
+++ test/integration/default/serverspec/run-local-tests.sh
@@ -23,10 +23,10 @@ bash -l -c "rvm install $v"
 bash -l -c "rvm use $v"
 bash -l -c "rvm use $v --default"
 bash -l -c "gem install bundler"
-bash -l -c "sudo -u nobody bundle install --path ./gems"
+bash -l -c "rvmsudo -u nobody bundle install --path ./gems"
 if [ "X$USER" != "Xroot" -a "X$USER" != "X" ]; then
     bash -l -c "env rvmsudo_secure_path=1 rvmsudo bundle exec rake spec"
 else
-    bash -l -c "sudo -u nobody bundle exec rake spec"
+    bash -l -c "rvmsudo -u nobody bundle exec rake spec"
 fi
 
