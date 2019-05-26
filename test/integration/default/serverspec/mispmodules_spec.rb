19edb98dd9194b220b46740357d1c482392e8807
juju4 <juju4@users.noreply.github.com>
2019-05-25 20:57:42 -0400

    test/default*: force LANG=C for misp-modules check

--- test/integration/default/serverspec/mispmodules_spec.rb
+++ test/integration/default/serverspec/mispmodules_spec.rb
@@ -49,6 +49,7 @@ describe command("/var/www/MISP/venv/bin/misp-modules -t"), :if => os[:family] =
 end
 
 describe command("/var/www/MISP/venv/bin/misp-modules -t"), :if => os[:family] == 'redhat' do
+  let(:pre_command) { 'export LANG=C' }
   its(:stdout) { should_not match /ERROR/ }
   its(:stdout) { should_not match /WARNING/ }
   let(:sudo_options) { '-u www-data -H' }
