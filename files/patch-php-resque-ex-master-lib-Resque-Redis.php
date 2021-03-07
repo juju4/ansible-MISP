3139dcd804bdb683452f8e675f7e28307e2f8d16
juju4 <juju4@users.noreply.github.com>
2021-03-07 13:48:19 -0500

    snuffleupagus: strict casting for app/Vendor/kamisama/php-resque-ex/lib/Resque/Redis.php (3)

--- files/patch-php-resque-ex-master-lib-Resque-Redis.php
+++ files/patch-php-resque-ex-master-lib-Resque-Redis.php
@@ -1,13 +1,11 @@
-diff --git a/Redis.php.0 b/Redis.php
-index 4d53402..01b81ad 100644
---- a/Redis.php.0
-+++ b/Redis.php
-@@ -19,7 +19,7 @@ if (class_exists('Redis')) {
+--- Redis.php.0	2021-03-07 17:22:34.893788318 +0000
++++ Redis.php	2021-03-07 17:24:35.120737618 +0000
+@@ -20,7 +20,7 @@
  
-         function establishConnection()
-         {
--            $this->pconnect($this->host, (int)$this->port, (int)$this->timeout, getmypid());
-+            $this->pconnect($this->host, (int)$this->port, (int)$this->timeout, (string)getmypid());
-             if ($this->password !== null) {
-                 $this->auth($this->password);
-             }
+ 		function establishConnection()
+ 		{
+-			$this->pconnect($this->host, (int) $this->port, (int) $this->timeout, getmypid());
++			$this->pconnect($this->host, (int) $this->port, (int) $this->timeout, (string)getmypid());
+ 			if ($this->password !== null) {
+ 				$this->auth($this->password);
+ 			}
