9d8977775e25d1b153b92d34834982b4f0be709d
juju4 <juju4@users.noreply.github.com>
2022-02-05 12:45:19 -0500

    workflow/lxd: use Centos 8-Stream

--- test/lxd/centos-ssh-image.sh
+++ test/lxd/centos-ssh-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # add ssh to default lxd image
 
-if [ "X$1" = "X8" ]; then
+if [ "X$1" = "X8" -o "X$1" = "X8-Stream" ]; then
   image=centos-8
 else
   image=centos-7
@@ -16,7 +16,7 @@ openssl rand -base64 48 | perl -ne 'print "$_" x2' | lxc exec $guest -- passwd r
 
 lxc exec $guest -- dhclient eth0
 lxc exec $guest -- ping -c 1 8.8.8.8
-if [ "X$1" = "X8" ]; then
+if [ "X$1" = "X8" -o "X$1" = "X8-Stream" ]; then
   lxc exec $guest -- dnf -y upgrade
   lxc exec $guest -- dnf install -y openssh-server sudo ruby yum-utils
   lxc exec $guest -- dnf install -y python3 python3-pip openssl-devel python36-devel libffi-devel "@Development tools"
