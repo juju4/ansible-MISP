f5a934d2a95ae89f7cda87d4d460554c888e95eb
juju4 <juju4@users.noreply.github.com>
2023-02-18 13:32:49 +0000

    Revert "ci(test-kitchen): skip serverspec tests associated to pymisp"
    This reverts commit 4b4030486223cb3fdc0542d8a0c46da07a6750bd.

--- test/integration/default/serverspec/misp2_spec.rb
+++ test/integration/default/serverspec/misp2_spec.rb
@@ -7,7 +7,6 @@ misp_rootdir = '/var/www/_MISP/MISP'
 misp_virtualenv = '/var/www/_MISP/venv'
 
 describe command("cd #{misp_rootdir}/tests && ./curl_tests.sh `cat /var/www/_MISP/MISP/.ht_key` | tee /tmp/curl_tests.out") do
-  skip("Pymisp issue inside github actions only")
 ## FIXME! is it normal first run get 'HTTP/1.1 500 Internal Server Error', second run: 'HTTP/1.1 100 Continue' + 'HTTP/1.1 302 Found'
 ##	not like https://travis-ci.org/MISP/MISP/jobs/128166085
   its(:stdout) { should match /548847db-060c-4275-a0c7-15bb950d210b/}
@@ -28,7 +27,6 @@ end
 #end
 
 describe command("cd #{misp_rootdir}/PyMISP && #{misp_virtualenv}/bin/python tests/test.py | tee /tmp/tests.out") do
-  skip("Pymisp issue inside github actions only")
   its(:exit_status) { should eq 0 }
   let(:sudo_options) { '-u _misp -H' }
 end
