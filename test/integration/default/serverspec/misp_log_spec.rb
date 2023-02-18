09bcd6b92b50a272c947a6e00d183e436dab19f6
juju4 <juju4@users.noreply.github.com>
2023-02-18 14:24:08 +0000

    ci(default-kitchen): ignore snuffleupagus warnings

--- test/integration/default/serverspec/misp_log_spec.rb
+++ test/integration/default/serverspec/misp_log_spec.rb
@@ -8,7 +8,7 @@ misp_rootdir = '/var/www/_MISP/MISP'
 describe file("#{misp_rootdir}/app/tmp/logs/error.log") do
 #  its(:size) { should > 0 }
 #  its(:content) { should_not match /Error:/ }
-  its(:content) { should_not match /Warning:/ }
+  its(:content) { should_not match /Warning:((?!\[snuffleupagus\]).)*/i }
 end
 
 #describe file("#{misp_rootdir}/app/tmp/logs/resque-worker-error.log") do
